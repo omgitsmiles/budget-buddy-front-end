ac4235e5720045a9037351552bcea248331ff142
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2023-08-16 15:18:26 -0400

    updating 404

--- src/components/Home.js
+++ src/components/Home.js
@@ -85,7 +85,27 @@ export default function Home({ setUser, user }) {
       })
     }
 
-    if (!user) return <Lottie loop={true} animationData={budgetLoading}/>
+    if (!user) return (
+      <>
+         <Heading
+        as={motion.h1}
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: -100 }}
+        fontWeight={600}
+        fontSize={{ base: '3xl', sm: '4xl', md: '5xl', lg: '6xl' }}
+        mb={'25'}
+        lineHeight={'110%'}>
+        <Text as={'span'} 
+            bgGradient="linear(to-r, green.400,green.700)"
+            bgClip="text"
+            >
+            404 Budget Not Found
+        </Text>
+      </Heading>
+    <Lottie loop={true} animationData={budgetLoading} />
+      </>
+    )
 
   return (
     <>
